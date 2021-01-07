cdbedfd30d936da6efdd4353e7d1f591b73c3acb
Aline <alinefrez@netscape.net>
2021-01-06 23:03:52 -0300

    Correção da identação

--- Trabalhos/20210107 Tarefa 2 - Problema das Jarras.pl
+++ Trabalhos/20210107 Tarefa 2 - Problema das Jarras.pl
@@ -16,34 +16,34 @@ objetivo((J1, 2)). % objetivo( (Qual jarra, Quantos litros) )
 % b)
 % creio que não posso deixar transbordar
 
-acao((J1, J2), encher1, (4, J2)) :- J1 < 4.    % Jarra 1 fica totalmente cheia e jarra 2 não se altera
-acao((J1, J2), encher2, (J1, 3)) :- J2 < 3.    % Jarra 2 fica totalmente cheia e jarra 2 não se altera
-acao((J1, J2), esvaziar1, (0, J2)) :- J1 > 0.  % Jarra 1 com o totalmente vazia e jarra 1 não se altera
-acao((J1, J2), esvaziar2, (J1, 0)) :- J2 > 0.  % Jarra 2 com o totalmente vazia e jarra 1 não se altera
+acao((J1, J2), encher1, (4, J2)) :- J1 < 4.	% Jarra 1 fica totalmente cheia e jarra 2 não se altera
+acao((J1, J2), encher2, (J1, 3)) :- J2 < 3.	% Jarra 2 fica totalmente cheia e jarra 2 não se altera
+acao((J1, J2), esvaziar1, (0, J2)) :- J1 > 0.	% Jarra 1 com o totalmente vazia e jarra 1 não se altera
+acao((J1, J2), esvaziar2, (J1, 0)) :- J2 > 0.	% Jarra 2 com o totalmente vazia e jarra 1 não se altera
 
 acao((J1, J2), passar12, (J1a, J2a)) :- 
-    J1 > 0,									% Não pode estar totalmente vazia
-    J2 < 3,									% Não pode estar totalmente cheia
+    J1 > 0,					% Não pode estar totalmente vazia
+    J2 < 3,					% Não pode estar totalmente cheia
     3 - J2 < J1,
     J1a is J1 - (3 - J2),
     J2a is J2 + (3 - J2).
 
 acao((J1, J2), passar12, (J1a, J2a)) :-
-    J1 > 0,									% Não pode estar totalmente vazia
-    J2 < 3,									% Não pode estar totalmente cheia
+    J1 > 0,					% Não pode estar totalmente vazia
+    J2 < 3,					% Não pode estar totalmente cheia
     3 - J2 >= J1,
-    J1a is 0,									%lida com o problema da jarra 1 com litro negativo
+    J1a is 0,					%lida com o problema da jarra 1 com litro negativo
     J2a is J2 + J1.
 
 acao((J1, J2), passar21, (J1a, J2a)) :-
-    J1 < 4,									% Não pode estar totalmente cheia
-    J2 > 0,									% Não pode estar totalmente vazia
+    J1 < 4,					% Não pode estar totalmente cheia
+    J2 > 0,					% Não pode estar totalmente vazia
     4 - J1 < J2,
     J1a is J1 + (4 - J1),
     J2a is J2 - (4 - J1).
 
 acao((J1, J2), passar21, (J1a, J2a)) :-
-    J1 < 4,									% Não pode estar totalmente cheia
-    J2 > 0,									% Não pode estar totalmente vazia
+    J1 < 4,					% Não pode estar totalmente cheia
+    J2 > 0,					% Não pode estar totalmente vazia
     4 - J1 >= J2,
-    J2a is 0,									%lida com o problema da jarra 2 com litro negativo
+    J2a is 0,					%lida com o problema da jarra 2 com litro negativo
