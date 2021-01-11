02abd5d60643f16299765bf5954682504e6f7e97
Aline <alinefrez@netscape.net>
2021-01-11 15:16:14 -0300

    Iniciei a tarefa 3 com o estado final

---
+++ Trabalhos/20210112 Tarefa 3 - Jogo dos 15.pl
@@ -0,0 +1,11 @@
+% Tarefa 3 - Jogo dos 15 (2020.01.12)
+
+estado( [[A,B,C,D],
+	 [E,F,G,H],
+	 [I,J,K,L],
+	 [M,N,O,b]]). % b é espaço em branco
+
+estadoFinal([[1, 2, 3, 4],
+             [5, 6, 7, 8],
+	     [9,10,11,12],
+	     [13,14,15,b]]).
